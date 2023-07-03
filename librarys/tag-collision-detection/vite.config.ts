cf5af76342c0d851ed11e5cf8cc4fdff6905a89a
huangyingwen <hyw.huangyingwen@gmail.com>
2023-07-03 13:28:18 +0800

    refactor: 修改 sdk 目录

--- librarys/tag-collision-detection/vite.config.ts
+++ librarys/tag-collision-detection/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 import DefineOptions from 'unplugin-vue-define-options/vite';
 import { resolve } from 'path';
@@ -9,29 +8,23 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'button',
+      name: 'library',
       fileName: 'index',
       formats: ['es', 'umd', 'cjs'],
     },
     rollupOptions: {
-      //忽略打包vue文件
-      external: ['vue'],
+      // 忽略打包vue文件
       output: {
-        globals: {
-          vue: 'Vue',
-        },
         dir: 'dist',
       },
     },
     sourcemap: true,
   },
   plugins: [
-    vue(),
-
     dts({
       entryRoot: './src',
       outputDir: ['./dist'],
-      //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      // 指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
       tsConfigFilePath: 'tsconfig.json',
     }),
     DefineOptions(),
